3fa1e61bc379c02b9724c8dcd9e22ad4eeae34bf
Alvaro Lockhart <alvarolock@gmail.com>
2016-10-31 15:28:26 -0400

    Views designed

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Alvaro De La Cruz on 10/31/16.
+//  Copyright © 2016 Alvaro De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,17 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by Alvaro De La Cruz on 10/31/16.
+//  Copyright © 2016 Alvaro De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
